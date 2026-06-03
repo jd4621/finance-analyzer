5f59d4b5a44c701c52c59dc22288fce481ab7b6e
John Denge <dengejohn4@gmail.com>
2026-06-03 15:47:37 +0300

    Updated

--- visualizer.py
+++ visualizer.py
@@ -2,4 +2,15 @@ import plotly.express as px
 import pandas as pd
 
 def spending_by_category_chart(df: pd.DataFrame):
-    
+    expenses = df[df["Type"] == "Expense"].groupby("Category")["AbsAmount"].sum().reset_index()
+    fig = px.pie(
+        expenses,
+        values="AbsAmount",
+        names="Category",
+        title="Spending by Category",
+        hole=0.4
+    )
+
+    fig.update_layout(
+        legend=dict(orientation="h", y=-0.2)
+    )
