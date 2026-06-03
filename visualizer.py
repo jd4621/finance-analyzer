407ac4b48c300cc4cd8557e6733c4e3c2d3e9ab3
John Denge <dengejohn4@gmail.com>
2026-06-03 16:43:15 +0300

    Updated

--- visualizer.py
+++ visualizer.py
@@ -14,3 +14,31 @@ def spending_by_category_chart(df: pd.DataFrame):
     fig.update_layout(
         legend=dict(orientation="h", y=-0.2)
     )
+
+    return fig
+
+def spending_over_time_chart(df: pd.DataFrame):
+    daily = df[df["Type"] == "Expense"].groupby("Date")["AbsAmount"].sum().reset_index()
+    fig = px.line(
+        daily,
+        x="Date",
+        y="AbsAmount",
+        title="Daily Spending over Time",
+        labels={"ABsAmount": "Amount (KES)", "Date": "Date"},
+    )
+
+    fig.update_traces(line_color="#e74c3c")
+    return fig
+
+
+def top_expenses_chart(df: pd.DataFrame):
+    top = df[df["Type"] == "Expense"].nlargest(10, "AbsAmount")
+    fig = px.bar(
+        x="AbsAmount",
+        y="Description",
+        orientation="h",
+        title="Top 10 Expenses",
+        labels={"AbsAMount": "Amount (KES)", "Description": ""},
+    )
+
+    fig.update_traces()
