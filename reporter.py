80ad57c25fc2619f7d8434b05c61ddcb1d2d045f
John Denge <dengejohn4@gmail.com>
2026-06-11 13:09:18 +0300

    Updated

--- reporter.py
+++ reporter.py
@@ -1,2 +1,8 @@
 from fpdf import FPDF
 from datetime import datetime
+
+def generate_pdf_report(summary: dict, category_totals: dict,
+                        budgets: dict, savings_goal: dict) -> bytes:
+    pdf = FPDF()
+    pdf.add_page()
+
